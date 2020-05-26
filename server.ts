5dab7eda6bfbb8ffdf93aacc82167a374149b057
weilyuwang <weilyuwang@gmail.com>
2020-05-26 19:09:30 -0400

    extract routes to a separate file

--- server.ts
+++ server.ts
@@ -1,18 +1,14 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
+import router from "./routes.ts";
+
 const port = 5000;
 
 const app = new Application();
 
-const router = new Router();
-
 // middleware
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-router.get("/api/v0/products", ({ response }: { response: any }) => {
-  response.body = "Hello World";
-});
 
 console.log(`Server running on port ${port}`);
 
