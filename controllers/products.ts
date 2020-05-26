1c93449301548f34c8c7541923c56ed4d7ae4883
weilyuwang <weilyuwang@gmail.com>
2020-05-26 19:13:19 -0400

    create Product interface and products data

---
+++ controllers/products.ts
@@ -0,0 +1,22 @@
+import Product from "../types.ts";
+
+let products: Product[] = [
+  {
+    id: "1",
+    name: "Product One",
+    description: "This is product one",
+    price: 29.99,
+  },
+  {
+    id: "2",
+    name: "Product Two",
+    description: "This is product two",
+    price: 39.99,
+  },
+  {
+    id: "3",
+    name: "Product Three",
+    description: "This is product three",
+    price: 59.99,
+  },
+];
