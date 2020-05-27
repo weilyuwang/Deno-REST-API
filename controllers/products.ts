d8b972f2615b9aa422c82b12680b503226a1f836
weilyuwang <weilyuwang@gmail.com>
2020-05-26 21:58:39 -0400

    implement post request

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,4 +1,5 @@
 import Product from "../types.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let products: Product[] = [
   {
@@ -53,8 +54,26 @@ const getProduct = (
 
 // @desc    Add product
 // @route   POST /api/v0/products
-const addProduct = ({ response }: { response: any }) => {
-  response.body = "add";
+const addProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      message: "No data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
 };
 
 // @desc    Update product
